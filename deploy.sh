48f21512d7806cf7221eb5f95f50660655cde56a
Forest Belton <forest@homolo.gy>
2017-04-18 23:37:57 -0700

    Add deploy script

---
+++ deploy.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -e
+
+if [ "$HOST" = "" ]; then
+    echo "error: \$HOST not defined" 2>&1
+    exit 1
+fi
+
+stack exec site clean
+stack clean
+
+stack build
+stack exec site build
+
+ssh $HOST "sudo rm -rf /tmp/_site"
+scp -r _site $HOST:/tmp/_site
+
+ssh $HOST <<EOF
+    sudo chown -R www-data:www-data /tmp/_site \
+    && sudo rm -rf /opt/_site \
+    && sudo mv /tmp/_site /opt/_site
+EOF
