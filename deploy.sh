3b062f4884246e9b1a0f2865390bca7a448d9194
Forest Belton <forest@homolo.gy>
2017-04-24 20:35:07 -0700

    Preserve LD38 directory on deploy

--- deploy.sh
+++ deploy.sh
@@ -7,17 +7,21 @@ if [ "$HOST" = "" ]; then
     exit 1
 fi
 
+echo "===> Building site"
 stack exec site clean
 stack clean
 
 stack build
 stack exec site build
 
+echo "==> Copying in new site"
 ssh $HOST "sudo rm -rf /tmp/_site"
 scp -r _site $HOST:/tmp/_site
 
+echo "==> Updating symlinks and permissions"
 ssh $HOST <<EOF
     sudo ln -s /opt/pdf /tmp/_site/pdf \
+    && sudo ln -s /opt/secret /tmp/_site/secret \
     && sudo chown -R www-data:www-data /tmp/_site \
     && sudo rm -rf /opt/_site \
     && sudo mv /tmp/_site /opt/_site
