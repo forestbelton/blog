a5ff871a246b65e1591ae58c747d7b31e7ea12a9
Forest Belton <forest@homolo.gy>
2017-10-16 15:17:09 -0700

    Support better LaTeX rendering through MathJax

--- site.hs
+++ site.hs
@@ -1,8 +1,9 @@
 --------------------------------------------------------------------------------
 {-# LANGUAGE OverloadedStrings #-}
 import           Data.Monoid (mappend)
+import qualified Data.Set as S
 import           Hakyll
-
+import           Text.Pandoc.Options
 
 --------------------------------------------------------------------------------
 main :: IO ()
@@ -21,13 +22,13 @@ main = hakyll $ do
 
     match (fromList ["about.rst", "contact.markdown"]) $ do
         route   $ setExtension "html"
-        compile $ pandocCompiler
+        compile $ pandocMathCompiler
             >>= loadAndApplyTemplate "templates/default.html" defaultContext
             >>= relativizeUrls
 
     match "posts/*" $ do
         route $ setExtension "html"
-        compile $ pandocCompiler
+        compile $ pandocMathCompiler
             >>= loadAndApplyTemplate "templates/post.html"    postCtx
             >>= saveSnapshot "content"
             >>= loadAndApplyTemplate "templates/default.html" postCtx
@@ -85,6 +86,16 @@ feedConfiguration = FeedConfiguration
     , feedDescription = "math, computer science, programming, and anywhere in between"
     , feedAuthorName = "Forest Belton"
     , feedAuthorEmail = "forest@homolo.gy"
-    , feedRoot = "https://homolo.gy" 
+    , feedRoot = "https://homolo.gy"
     }
 
+--------------------------------------------------------------------------------
+pandocMathCompiler :: Compiler (Item String)
+pandocMathCompiler = pandocCompilerWith defaultHakyllReaderOptions writerOptions
+    where mathExtensions = [Ext_tex_math_dollars, Ext_tex_math_double_backslash, Ext_latex_macros]
+          defaultExtensions = writerExtensions defaultHakyllWriterOptions
+          newExtensions = foldr S.insert defaultExtensions mathExtensions
+          writerOptions = defaultHakyllWriterOptions
+                              { writerExtensions = newExtensions
+                              , writerHTMLMathMethod = MathJax ""
+                              }
